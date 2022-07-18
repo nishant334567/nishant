a5d001a3942cd7a6213c0715b6a39f272ee71acc
Nishant Thakur <nishantkumar334567@gmail.com>
2022-07-18 17:57:00 +0530

    edits

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,28 @@
 import React,{useState} from 'react'
 import axios from'axios'
+// import emailjs from 'emailjs-com'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
-  const [title,setTitle]=useState('');
+    const [title,setTitle]=useState('');
     const [message,setMessage]=useState("");
-    async function submitQuery(e){
-        e.preventDefault();
-        console.log(title,message)
-        await axios.post('http://localhost:5000/query', {
-            title: title,
-            message: message
-        }).then(res=>console.log(res))
+
+
+    function submitQuery(e){
+      // console.log(templateParams);
+      emailjs.sendForm('service_thehahp', 'template_1zdld8n', '#form','2qgl64Y8RnOw6iWRn')
+      .then((result) => {
+          console.log(result.text);
+          // console.log(result)
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.preventDefault();
+      // e.target.reset();
     }
   return (
     <>
+    
 
         <div className='contact__heading'>
                 <h1>Get in touch !!</h1>
@@ -23,9 +32,11 @@ function Contact() {
         <div id='contact_jump' className='contact'>
             
             <div className='contact-form'>
-                <form className='actual-form' onSubmit={submitQuery} >
-                    <input placeholder='Title' value={title} onChange={e=>setTitle(e.target.value)}/>
-                    <textarea placeholder='Message' value={message} onChange={e=>setMessage(e.target.value)}></textarea>
+                <form id='form' className='actual-form' onSubmit={submitQuery} >
+                    <input placeholder='Title' name='title' value={title} onChange={e=>setTitle(e.target.value)}/>
+                    <input placeholder='Name' name='name'></input>
+                    {/* <input type='email' placeholder='Email' name='email'></input> */}
+                    <textarea placeholder='Message' name='mess' value={message} onChange={e=>setMessage(e.target.value)}></textarea>
                     <button type='submit'>Send message</button>
                     
                 </form>
--- src/components/Project.js
+++ src/components/Project.js
@@ -12,6 +12,8 @@ function Project() {
         <h2>Projects</h2>
     </div>
     <div className='projects'>
+    <Cards link='https://bit.ly/nishant-crud-app' tech="Express NodeJS MongoDB" title="CRUD App" description="I made this basic CRUD application during my placements, to put it in my resume. User can Create, Update and Delete post with Authentication feature."/>
+
         <Cards link='http://nishant-thakur.herokuapp.com' tech="ReactJS HTML CSS" title="My Portfolio" description="I made my own portfolio website using the above technologies. " />
         <Cards link='https://bit.ly/nishant-crud-app' tech="Express NodeJS MongoDB" title="CRUD App" description="I made this basic CRUD application during my placements, to put it in my resume. User can Create, Update and Delete post with Authentication feature."/>
     </div>
